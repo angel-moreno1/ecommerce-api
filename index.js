57f21b04dc9c484e810dfe5be257771375ce302a
Angel Moreno <angelmrsofa@gmail.com>
2021-04-13 23:15:20 -0500

    adding workflow

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ import express from 'express';
 import cors from 'cors';
 import http from 'http';
 import usersRoute from './routes/users.js';
-
-const app = express();
-const server = http.createServer(app);
+import { Server } from 'socket.io';
 
 const PORT = process.env.PORT || 3000;
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', usersRoute);
+
+const server = http.createServer(app);
+const io = new Server(server, {cors: { origin: '*' }}) ;
+
+io.on('connection', socket => {
+    console.log('ya');
+
+    socket.on('disconnect', () => {
+        console.log('disconected');
+    });
+});
 
 server.listen(PORT, () => void console.log('runnning at', PORT));
